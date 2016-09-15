01fb3622f04896c7fa6643d1a9a3aaf2d3488542
Teffen Ellis (Eric) <teffen@nirri.us>
2016-09-15 12:48:44 -0700

    Flesh out app boilerplate.

---
+++ scripts/watch.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+
+const {hostname, port, protocol} = require("../package.json").routes.development
+const webpack = require("webpack")
+const WebpackDevServer = require("webpack-dev-server")
+const config = require("../webpack.config.js")
+
+const server = new WebpackDevServer(webpack(config), {
+  historyApiFallback: true,
+  publicPath: config.output.publicPath,
+  stats: {
+    assets: true,
+    chunks: false,
+    chunkModules: false,
+    colors: true,
+    hash: false,
+    timings: true,
+    version: false
+  }
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Listening on ${protocol}://${hostname}:${port} (localhost)`)
+  console.log("Starting server...")
+})
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+/* eslint-env node */
+"use strict"
+
+const ENVIRONMENT = process.env.NODE_ENV || "development"
+const {resolve} = require("path")
+const {description, routes, version} = require("./package.json")
+const {port} = routes[ENVIRONMENT]
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const marked = require("marked")
+const autoprefixer = require("autoprefixer")
+
+const exclude = /node_modules/
+
+const renderer = new marked.Renderer()
+const $ = {}
+
+$.buildDirectory = "site-deploy"
+
+$.devtool = "source-map"
+
+$.entry = {
+  site: "./app/index.js"
+  // segment: "./app/segment.js" TODO get account key
+}
+
+$.markdownLoader = {renderer}
+
+$.output = {
+  filename: "[name].js",
+  sourceMapFilename: "[name].map"
+}
+
+$.plugins = [
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    VERSION: JSON.stringify(version),
+    "process.env.NODE_ENV": JSON.stringify(ENVIRONMENT)
+  }),
+  new HtmlWebpackPlugin({
+    title: "Instant Plugin",
+    description,
+    template: "app/index.pug"
+  })
+]
+
+$.resolve = {
+  extensions: ["", ".js", ".json"],
+  modules: [resolve(__dirname, "app"), "node_modules"]
+}
+
+$.postcss = () => [autoprefixer({remove: false, browsers: ["last 2 versions", "ie 10"]})]
+
+const minimizeParam = ENVIRONMENT === "development" ? "-minimize" : "minimize"
+
+$.module = {
+  loaders: [
+    {test: /\.pug$/, loader: "pug", exclude},
+    {test: /\.png|jpe?g|gif$/i, loader: "url?limit=0", exclude},
+    {test: /\.js$/, loader: "babel", exclude},
+    {test: /\.svg$/, loader: "svg-inline", exclude},
+    {test: /\.styl$/, loader: `style!css?${minimizeParam}!postcss!stylus?paths=app`}
+  ],
+  noParse: /\.min\.js/
+}
+
+if (ENVIRONMENT === "development") {
+  $.devtool = "eval"
+
+  $.module.preLoaders = [{
+    exclude,
+    loader: "eslint-loader",
+    test: /\.js$/
+  }]
+
+  const devServerClient = `webpack-dev-server/client?http://0.0.0.0:${port}`
+
+  if (Array.isArray($.entry)) {
+    $.entry.unshift(devServerClient)
+  }
+  else {
+    $.entry["dev-server-client"] = devServerClient
+  }
+}
+
+module.exports = $
