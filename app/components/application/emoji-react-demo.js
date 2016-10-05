568059c035aed00be8e9572d6cc6b22ab8c04162
Teffen Ellis (Eric) <teffen@nirri.us>
2016-10-04 23:34:00 -0700

    Flesh out emoji react demo.

---
+++ app/components/application/emoji-react-demo.js
@@ -0,0 +1,23 @@
+
+const script = `<script type="text/javascript">
+  var emojis = "tada, fire, grinning"
+  var selector = "body"
+  var url = window.location.href.replace(/(http:\/\/|https:\/\/)/gi, '').replace(/^\/|\/$/g, '');
+
+  var iframe = document.createElement("iframe")
+  iframe.src = "https://emojireact.com/embed?emojis=" + emojis.replace(/\s/g, "") + "&url=" + url
+  iframe.scrolling = "no"
+  iframe.frameBorder = "0"
+  iframe.style = "border:none; overflow:hidden; width:130px; height:35px;"
+
+  var container = document.querySelector(selector)
+
+  container.insertBefore(iframe, container.firstChild || container)
+</script>`
+
+export default runDemo(app) {
+  const {embedCodeInput} = app.refs
+
+  embedCodeInput.value = script
+  app.parseInput()
+}
