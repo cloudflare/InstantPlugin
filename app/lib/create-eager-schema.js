d048ff9135332c2d4d600d08504eed19848346c9
Teffen Ellis (Eric) <teffen@nirri.us>
2016-10-07 02:43:29 -0700

    Replace deprecated functions.

--- app/lib/create-eager-schema.js
+++ app/lib/create-eager-schema.js
@@ -1,4 +1,8 @@
-const encode = string => window.btoa(unescape(encodeURIComponent(string)))
+function encode(string) {
+  return btoa(encodeURIComponent(string)
+    .replace(/%([0-9A-F]{2})/g, (match, p1) => String.fromCharCode("0x" + p1))
+  )
+}
 
 export default function createEagerSchema({embedCode, properties}) {
   const initializeApp = function initializeApp(encodedEmbedCode) {
@@ -6,9 +10,13 @@ export default function createEagerSchema({embedCode, properties}) {
 
     const TRACKED_ENTITY_PATTERN = /TRACKED_ENTITY\[(\S+)\]/g
     const options = INSTALL_OPTIONS
-
     const insertOption = (match, key) => options[key]
-    const decode = string => decodeURIComponent(escape(window.atob(string)))
+
+    function decode(string) {
+      return decodeURIComponent(Array.prototype.map.call(atob(string), c => {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
+      }).join(""))
+    }
 
     function insertEmbedCode() {
       const embedCode = decode(encodedEmbedCode).replace(TRACKED_ENTITY_PATTERN, insertOption)
